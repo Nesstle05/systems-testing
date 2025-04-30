4307d44a5755c560bcf6a96fe0a5d13d9de90c4a
Vanessa Cupsan <teodoravanessacupsan2004@gmail.com>
2025-04-30 13:19:02 +0300

    tes

--- laboratories/cicd-documentation/tree.py
+++ laboratories/cicd-documentation/tree.py
@@ -75,11 +75,17 @@ class Tree:
             self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
-        # TODO
-        pass
+        """Method for preorder traversal (Node -> Left -> Right)"""
+        if node is not None:
+            print(str(node.data) + ' ')
+            self._printPreorderTree(node.left)
+            self._printPreorderTree(node.right)
 
     def _printPostorderTree(self, node):
-        # TODO
-        pass
+        """Method for postorder traversal (Left -> Right -> Node)"""
+        if node is not None:
+            self._printPostorderTree(node.left)
+            self._printPostorderTree(node.right)
+            print(str(node.data) + ' ')
 
 
